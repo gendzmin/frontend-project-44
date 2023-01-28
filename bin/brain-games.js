9456139c916637c679d2e1e49aed6bfe9ecff6fd
gendzmin <gendzmin@gmail.com>
2023-01-29 00:17:07 +0300

    adding src/cli.js; greeting function

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
+import { greeting } from '../src/cli.js';
 console.log('Welcome to the Brain Games!');
-var readlineSync = require('readline-sync');
-var userName = readlineSync.question('May I have your name?');
-console.log(`Hello, ${userName}!`);
+greeting();
