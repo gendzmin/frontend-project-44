539a714cf14eeecce3e6799ab164fddf6ee3425e
gendzmin <gendzmin@gmail.com>
2023-01-28 23:49:49 +0300

    adding readline-sync

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,5 @@
 #!/usr/bin/env node
 console.log('Welcome to the Brain Games!');
+var readlineSync = require('readline-sync');
+var userName = readlineSync.question('May I have your name?');
+console.log(`Hello, ${userName}!`);
