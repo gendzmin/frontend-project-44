31a2cd66ee576e390f8033599c27a0b4093b9bce
gendzmin <113588320+gendzmin@users.noreply.github.com>
2023-02-03 22:45:34 +0300

    Update brain-prime-core.js

--- src/games/brain-prime-core.js
+++ src/games/brain-prime-core.js
@@ -1,9 +1,6 @@
 import { getRandomNum, gameTemplate } from '../index.js'; // Импортируем функцию-рандомайзер и шаблон из index.js
 
-const gameCore = () => {
-  const ruleSet = 'Answer "yes" if given number is prime. Otherwise answer "no". '; // Правила для игры в чётность
-  const question = getRandomNum(); // Объявление и инициализация вопроса - рандомного числа
-  const getPrimality = (num) => { // Функция, проверяющая простоту числа
+const getPrimality = (num) => { // Функция, проверяющая простоту числа
     const divisors = [2, 3, 5, 7];
     for (let i = 0; i !== 4; i += 1) {
       if (num % divisors[i] === 0 && !divisors.includes(num)) {
@@ -11,7 +8,10 @@ const gameCore = () => {
       }
     }
     return 'yes';
-  };
+};
+const gameCore = () => {
+  const ruleSet = 'Answer "yes" if given number is prime. Otherwise answer "no". '; // Правила для игры в чётность
+  const question = getRandomNum(); // Объявление и инициализация вопроса - рандомного числа
   const corAnswer = getPrimality(question); // // Объяв. и иниц. прав. ответа - строки `yes` / `no`
   return [ruleSet, question, corAnswer]; // Функция возвращает правила, вопрос и правильный ответ
 };
