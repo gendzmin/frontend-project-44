21bb3df46448e6e31a204a808877f555400dd998
gendzmin <113588320+gendzmin@users.noreply.github.com>
2023-02-03 23:55:37 +0300

    Update brain-progression-core.js

--- src/games/brain-progression-core.js
+++ src/games/brain-progression-core.js
@@ -1,19 +1,19 @@
 import { getRandomNum, gameTemplate } from '../index.js'; // Импортируем функцию-рандомайзер и шаблон из index.js
 
+const getProgSequence = (length, first, step) => { // Функция генерации последовательности
+  const sequence = [];
+  let currentNum = first;
+  for (let i = 0; i !== length; i += 1) {
+    sequence.push(currentNum);
+    currentNum += step;
+  }
+  return sequence;
+};
 const gameCore = () => {
   const ruleSet = 'What number is missing in the progression?'; // Правила для игры-прогрессии
   const progLength = getRandomNum(11, 6); // Генерация длины последовательности - от 6 до 10
   const progFirstNum = getRandomNum(); // Генерация первого числа последовательности
   const progStep = getRandomNum(11, 3); // Генерация шага последовательности - от 3 до 10
-  const getProgSequence = (length, first, step) => { // Функция генерации последовательности
-    const sequence = [];
-    let currentNum = first;
-    for (let i = 0; i !== length; i += 1) {
-      sequence.push(currentNum);
-      currentNum += step;
-    }
-    return sequence;
-  };
   const progSequence = getProgSequence(progLength, progFirstNum, progStep); // Переменная с последов
   const progAbsentNum = getRandomNum(progLength - 1); // Перем. со случ. элем. последов - не > длины
   const corAnswer = `${progSequence[progAbsentNum]}`; // Объявление и иниц. переменной правильного ответа
