676a4f4b5110dbf67fa53eb72dee66793f73ca27
gendzmin <113588320+gendzmin@users.noreply.github.com>
2023-02-03 23:58:47 +0300

    Update brain-gcd-core.js

--- src/games/brain-gcd-core.js
+++ src/games/brain-gcd-core.js
@@ -1,16 +1,16 @@
 import { getRandomNum, gameTemplate } from '../index.js'; // Импортируем функцию-рандомайзер и шаблон из index.js
 
+const getGrComDiv = (a, b) => { // Функция, находящся НОД
+  const c = a % b;
+  if (c === 0) {
+    return b;
+  } return getGrComDiv(b, c);
+};
 const gameCore = () => {
   const ruleSet = 'Find the greatest common divisor of given numbers.'; // Правила для игры-НОД
   const num1 = getRandomNum(100, 1); // Получение двух сгенерированных чисел
   const num2 = getRandomNum(num1, 1); // Второе число < первого - для корректной работы функции НОД
   const question = `${num1} ${num2}`; // Объявление и инициализация строки-вопроса
-  const getGrComDiv = (a, b) => { // Функция, находящся НОД
-    const c = a % b;
-    if (c === 0) {
-      return b;
-    } return getGrComDiv(b, c);
-  };
   const corAnswer = (`${getGrComDiv(num1, num2)}`); // Объявление и иниц. переменной правильного ответа
   return [ruleSet, question, corAnswer]; // Функция возвращает правила, вопрос и правильный ответ
 };
