6097a3e07511c9553dc778938115bd94c026668e
gendzmin <gendzmin@gmail.com>
2023-02-06 15:59:44 +0300

    making corrections

--- src/games/brain-calc-core.js
+++ src/games/brain-calc-core.js
@@ -1,27 +1,27 @@
 import gameTemplate from '../index.js'; // Импорт шаблона из index.js
 import getRandomNum from '../utils.js'; // Импорт функции-рандомизатора из utils.js
 
+const getCorrectAnswer = (operator, num1, num2) => { // Функ, возвр. результат вычисл. с выбр. опер.
+  switch (operator) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    default:
+      return num1 + num2;
+  }
+};
+
 const gameCore = () => {
   const ruleSet = 'What is the result of the expression?'; // Правила для игры-калькулятора
   const num1 = getRandomNum(); // Получение двух сгенерированных чисел
   const num2 = getRandomNum();
-  const operArr = ['+', '-', '*']; // Массив операторов
-  const operType = operArr[Math.floor(Math.random() * (3 - 0) + 0)]; // Генер. чис. 0-2 и выбор опер
-  const question = `${num1} ${operType} ${num2}`; // Объявление и инициализация строки-вопроса
-  let correctAnswer = 0; // Объявление переменной правильного ответа и её инициализация через switch
-  switch (operType) {
-    case '+':
-      correctAnswer = num1 + num2;
-      break;
-    case '-':
-      correctAnswer = num1 - num2;
-      break;
-    case '*':
-      correctAnswer = num1 * num2;
-      break;
-    default:
-      correctAnswer = num1 + num2;
-  }
+  const operators = ['+', '-', '*']; // Массив операторов
+  const operatorType = operators[getRandomNum(operators.length)]; // Генер. числа 0-2 и выбор опер.
+  const question = `${num1} ${operatorType} ${num2}`; // Объявление и инициализация строки-вопроса
+  const correctAnswer = getCorrectAnswer(operatorType, num2, num2); // Переменная с прав. ответом
   return [ruleSet, question, correctAnswer]; // Функция возвращает правила, вопрос и прав. ответ
 };
 
