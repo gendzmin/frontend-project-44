670289d15970e1a64891ede9c52e15776ef0c72d
gendzmin <gendzmin@gmail.com>
2023-02-07 12:56:28 +0300

    fixing brain-prime-core

--- src/games/brain-prime-core.js
+++ src/games/brain-prime-core.js
@@ -12,7 +12,12 @@ const getPrimality = (num) => { // Функция, проверяющая про
 const ruleSet = 'Answer "yes" if given number is prime. Otherwise answer "no". '; // Правила для игры в чётность
 const gameCore = () => {
   const question = getRandomNum(100); // Объявление и инициализация вопроса - рандомного числа
-  const correctAnswer = getPrimality(question) ? 'yes' : 'no'; // // Объявление и инициализация правильного ответа
+  const getCorrectAnswer = (boolean) => { // Функция, возвращающая 'yes' или 'no' для прав. ответа
+    if (boolean) {
+      return 'yes';
+    } return 'no';
+  };
+  const correctAnswer = getCorrectAnswer(getPrimality(question)); // Получение правильного ответа
   return [question, correctAnswer]; // Функция возвращает правила, вопрос и прав. ответ
 };
 
